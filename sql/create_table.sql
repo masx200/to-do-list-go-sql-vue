c9ddcb6db4c1ed9afa3099690d973d260e8a05a1
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-22 15:48:01 +0800

    Update create_table.sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,4 +3,15 @@
 create database if not exists todolist;
 
 -- 切换库
-use todolist
+use todolist;
+
+CREATE TABLE  if not exists `to_do_items`(
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `deleted_at` datetime(3) DEFAULT NULL,
+    `content` longtext NOT NULL,
+    `finished` tinyint(1) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_to_do_items_deleted_at` (`deleted_at`)
+) ENGINE = InnoDB AUTO_INCREMENT = 20 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
