18363f301312497564817ff9ae993e48244cbe14
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-22 21:27:42 +0800

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	// "fmt"
-	// "math"
 	"strconv"
-	// "time"
-
-	// "math/rand"
 
 	"gitee.com/masx200/to-do-list-go-sql-vue/backend/configs"
 	"gitee.com/masx200/to-do-list-go-sql-vue/backend/database"
@@ -19,7 +14,6 @@ func main() {
 	var LoadConfig = configs.LoadConfig
 	type ToDoItem = todoitem.ToDoItem
 
-	// rand.Seed(time.Now().Unix())
 	config := LoadConfig()
 	db := database.ConnectDatabase(config.Dsn, &ToDoItem{})
 	r := gin.Default()
@@ -43,12 +37,5 @@ func main() {
 
 	})
 	r.Run(":" + strconv.Itoa(config.Port))
-	// fmt.Println("CreateItem", operations.CreateItem(db, &ToDoItem{Content: "hello world!" + strconv.FormatInt((rand.Int63n(math.MaxInt64)), 10), Finished: false}))
-	// fmt.Println("FindItems")
-	// fmt.Println(operations.FindItems(db, []ToDoItem{}, 30))
-	// fmt.Println("CreateItem", operations.CreateItem(db, &ToDoItem{Content: "hello world!" + strconv.FormatInt((rand.Int63n(math.MaxInt64)), 10), Finished: false}))
-	// fmt.Println("UpdateItem", operations.UpdateItem(db, &ToDoItem{Content: "changed!" + strconv.FormatInt((rand.Int63n(math.MaxInt64)), 10), Finished: true}, 2))
-	// fmt.Println("FindItems")
-	// fmt.Println(operations.FindItems(db, []ToDoItem{}, 30))
-	// fmt.Println("DeleteItem", operations.DeleteItem(db, &ToDoItem{}, 1))
+
 }
