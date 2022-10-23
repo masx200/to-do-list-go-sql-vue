e9f77156ea4f71364e890ef53036fbbdfdc08356
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-23 15:45:36 +0800

    修改表结构

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,17 +1,16 @@
--- Active: 1666419568297@@127.0.0.1@3306@todolist
--- 创建库
 create database if not exists todolist;
 
--- 切换库
 use todolist;
 
-CREATE TABLE  if not exists `to_do_items`(
-    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-    `created_at` datetime(3) DEFAULT NULL,
-    `updated_at` datetime(3) DEFAULT NULL,
-    `deleted_at` datetime(3) DEFAULT NULL,
-    `content` longtext NOT NULL,
-    `finished` tinyint(1) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `idx_to_do_items_deleted_at` (`deleted_at`)
+CREATE TABLE if not exists `to_do_items`(
+    `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) NULL DEFAULT NULL,
+    `updated_at` datetime(3) NULL DEFAULT NULL,
+    `deleted_at` datetime(3) NULL DEFAULT NULL,
+    `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `finished` tinyint(1) NOT NULL,
+    `author` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `idx_to_do_items_deleted_at`(`deleted_at` ASC) USING BTREE,
+    INDEX `idx_to_do_items_author`(`author` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 20 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
