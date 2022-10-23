d3219c78aba706ec9c4342b9edbb457df1d09e97
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-23 18:56:40 +0800

    Update controller.go

--- backend/controllers/controller.go
+++ backend/controllers/controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"strconv"
 
 	"gitee.com/masx200/to-do-list-go-sql-vue/backend/database"
@@ -8,23 +9,34 @@ import (
 	"gorm.io/gorm"
 )
 
-
 func POSTItem[T any](r *gin.Engine, db *gorm.DB, prefix string, model *T) {
 	r.POST(prefix, func(c *gin.Context) {
-		var item T
-		err := c.ShouldBindJSON(&item)
+		var input map[string]any
+		err := c.ShouldBindJSON(&input)
 		if err != nil {
 			c.String(400, err.Error())
 			return
 		}
+		delete(input, "id")
+		str, err := json.Marshal(input)
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
+		var item T
 
+		err = json.Unmarshal(str, &item)
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
 		id, err := database.CreateItem(db, model, &item)
 		if err != nil {
 			c.String(500, err.Error())
+			return
 		} else {
 
-			var item = new(T)
-			res, err := database.GetItem(db, item, uint(id))
+			res, err := database.GetItem(db, model, uint(id))
 			if err != nil {
 				c.JSON(200, []map[string]interface{}{})
 			} else {
