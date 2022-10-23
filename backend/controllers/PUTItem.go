10c306ea8dfb42c4adea811da85a769e8c391369
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-23 20:42:41 +0800

    拆分文件

---
+++ backend/controllers/PUTItem.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"strconv"
+
+	"gitee.com/masx200/to-do-list-go-sql-vue/backend/database"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func PUTItem[T any](r *gin.Engine, db *gorm.DB, prefix string, model *T) {
+	r.PUT(prefix, func(c *gin.Context) {
+		qsid := c.Query("id")
+		if len(qsid) == 0 {
+			c.String(400, "expect id but not found")
+			return
+		}
+		id, err := strconv.Atoi(qsid)
+
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
+		var item = new(T)
+		err = c.ShouldBindJSON(&item)
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
+
+		err = database.UpdateItem(db, &item, uint(id))
+		if err != nil {
+			c.String(500, err.Error())
+			return
+		}
+		res, err := database.GetItem(db, item, uint(id))
+		if err != nil {
+			c.String(500, err.Error())
+		} else {
+			c.JSON(200, []map[string]any{res})
+		}
+		// return
+	})
+}
--- backend/controllers/controller.go
+++ backend/controllers/controller.go
@@ -1,53 +1,12 @@
 package controllers
 
 import (
-	"encoding/json"
 	"strconv"
 
 	"gitee.com/masx200/to-do-list-go-sql-vue/backend/database"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
-
-func POSTItem[T any](r *gin.Engine, db *gorm.DB, prefix string, model *T) {
-	r.POST(prefix, func(c *gin.Context) {
-		var input map[string]any
-		err := c.ShouldBindJSON(&input)
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		delete(input, "id")
-		str, err := json.Marshal(input)
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		var item T
-
-		err = json.Unmarshal(str, &item)
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		id, err := database.CreateItem(db, model, &item)
-		if err != nil {
-			c.String(500, err.Error())
-			return
-		} else {
-
-			res, err := database.GetItem(db, model, uint(id))
-			if err != nil {
-				c.JSON(200, []map[string]interface{}{})
-			} else {
-				c.JSON(200, []map[string]interface{}{res})
-			}
-			return
-
-		}
-		// return
-	})
-}
 
 func DELETEItem[T any](r *gin.Engine, db *gorm.DB, prefix string, model *T) {
 	r.DELETE(prefix, func(c *gin.Context) {
@@ -76,41 +35,6 @@ func DELETEItem[T any](r *gin.Engine, db *gorm.DB, prefix string, model *T) {
 			c.String(500, err.Error())
 		} else {
 			c.JSON(200, []map[string]interface{}{res})
-		}
-		// return
-	})
-}
-
-func PUTItem[T any](r *gin.Engine, db *gorm.DB, prefix string, model *T) {
-	r.PUT(prefix, func(c *gin.Context) {
-		qsid := c.Query("id")
-		if len(qsid) == 0 {
-			c.String(400, "expect id but not found")
-			return
-		}
-		id, err := strconv.Atoi(qsid)
-
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		var item = new(T)
-		err = c.ShouldBindJSON(&item)
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-
-		err = database.UpdateItem(db, &item, uint(id))
-		if err != nil {
-			c.String(500, err.Error())
-			return
-		}
-		res, err := database.GetItem(db, item, uint(id))
-		if err != nil {
-			c.String(500, err.Error())
-		} else {
-			c.JSON(200, []map[string]any{res})
 		}
 		// return
 	})
