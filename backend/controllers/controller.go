dbce2b2f790f678d1b0f9eb77900c8ee999f6879
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-23 12:48:05 +0800

    移动文件

---
+++ backend/controllers/controller.go
@@ -0,0 +1,149 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+import "gorm.io/gorm"
+import "strconv"
+import "gitee.com/masx200/to-do-list-go-sql-vue/backend/database"
+
+func GETItems[T any](r *gin.Engine, db *gorm.DB, prefix string) {
+	r.GET(prefix, func(c *gin.Context) {
+
+		qsid := c.Query("id")
+
+		if len(qsid) != 0 {
+			c.Abort()
+			id, err := strconv.Atoi(qsid)
+
+			if err != nil {
+				c.String(400, err.Error())
+				return
+			}
+			var item = new(T)
+			item, err = database.GetItem(db, item, uint(id))
+			if err != nil {
+				c.String(404, err.Error())
+			} else {
+				c.JSON(200, item)
+			}
+			return
+		} else {
+			c.Next()
+		}
+
+	},
+		func(c *gin.Context) {
+			qsid := c.Query("id")
+
+			if len(qsid) != 0 {
+				return
+			}
+			c.Abort()
+			qslimit := c.DefaultQuery("limit", "50")
+
+			limit, err := strconv.Atoi(qslimit)
+			if err != nil {
+				c.String(400, err.Error())
+				return
+			}
+			qspage := c.DefaultQuery("page", "0")
+
+			page, err := strconv.Atoi(qspage)
+
+			if err != nil {
+				c.String(400, err.Error())
+				return
+			}
+			tdi, err := database.FindItems(db, []T{}, limit, page)
+			if err != nil {
+				c.String(500, err.Error())
+			} else {
+				c.JSON(200, tdi)
+			}
+			// return
+		})
+}
+
+func POSTItem[T any](r *gin.Engine, db *gorm.DB, prefix string) {
+	r.POST(prefix, func(c *gin.Context) {
+		var item T
+		err := c.ShouldBindJSON(&item)
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
+		err = database.CreateItem(db, &item)
+		if err != nil {
+			c.String(500, err.Error())
+		} else {
+			c.JSON(200, item)
+		}
+		// return
+	})
+}
+
+func DELETEItem[T any](r *gin.Engine, db *gorm.DB, prefix string) {
+	r.DELETE(prefix, func(c *gin.Context) {
+		qsid := c.Query("id")
+		if len(qsid) == 0 {
+			c.String(400, "expect id but not found")
+			return
+		}
+		id, err := strconv.Atoi(qsid)
+
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
+		var item = new(T)
+		item, err = database.GetItem(db, item, uint(id))
+		/* 保持接口的幂等性 */
+		if err != nil {
+			c.JSON(200, gin.H{
+				"id": id,
+			})
+			return
+		}
+		err = database.DeleteItem(db, new(T), uint(id))
+		if err != nil {
+			c.String(500, err.Error())
+		} else {
+			c.JSON(200, item)
+		}
+		// return
+	})
+}
+
+func PUTItem[T any](r *gin.Engine, db *gorm.DB, prefix string) {
+	r.PUT(prefix, func(c *gin.Context) {
+		qsid := c.Query("id")
+		if len(qsid) == 0 {
+			c.String(400, "expect id but not found")
+			return
+		}
+		id, err := strconv.Atoi(qsid)
+
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
+		var item = new(T)
+		err = c.ShouldBindJSON(&item)
+		if err != nil {
+			c.String(400, err.Error())
+			return
+		}
+
+		err = database.UpdateItem(db, &item, uint(id))
+		if err != nil {
+			c.String(500, err.Error())
+			return
+		}
+		item, err = database.GetItem(db, item, uint(id))
+		if err != nil {
+			c.String(404, err.Error())
+		} else {
+			c.JSON(200, item)
+		}
+		// return
+	})
+}
--- backend/routers/todorouter.go
+++ backend/routers/todorouter.go
@@ -1,9 +1,7 @@
 package routers
 
 import (
-	"strconv"
-
-	"gitee.com/masx200/to-do-list-go-sql-vue/backend/database"
+	"gitee.com/masx200/to-do-list-go-sql-vue/backend/controllers"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -11,143 +9,9 @@ import (
 
 func TodoRoute[T any](r *gin.Engine, db *gorm.DB, prefix string) {
 
-	r.GET(prefix, func(c *gin.Context) {
+	controllers.GETItems[T](r, db, prefix)
+	controllers.POSTItem[T](r, db, prefix)
 
-		qsid := c.Query("id")
-
-		if len(qsid) != 0 {
-			c.Abort()
-			id, err := strconv.Atoi(qsid)
-
-			if err != nil {
-				c.String(400, err.Error())
-				return
-			}
-			var item = new(T)
-			item, err = database.GetItem(db, item, uint(id))
-			if err != nil {
-				c.String(404, err.Error())
-			} else {
-				c.JSON(200, item)
-			}
-			return
-		} else {
-			c.Next()
-		}
-
-	}, func(c *gin.Context) {
-		qsid := c.Query("id")
-
-		if len(qsid) != 0 {
-			return
-		}
-
-		qslimit := c.DefaultQuery("limit", "50")
-
-		limit, err := strconv.Atoi(qslimit)
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		qspage := c.DefaultQuery("page", "0")
-
-		page, err := strconv.Atoi(qspage)
-
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		tdi, err := database.FindItems(db, []T{}, limit, page)
-		if err != nil {
-			c.String(500, err.Error())
-		} else {
-			c.JSON(200, tdi)
-		}
-		// return
-	})
-	r.POST(prefix, func(c *gin.Context) {
-		var item T
-		err := c.ShouldBindJSON(&item)
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		err = database.CreateItem(db, &item)
-		if err != nil {
-			c.String(500, err.Error())
-		} else {
-			c.JSON(200, item)
-		}
-		// return
-	})
-
-	DELETEItem[T](r, db, prefix)
-	PUTItem[T](r, db, prefix)
-}
-
-func DELETEItem[T any](r *gin.Engine, db *gorm.DB, prefix string) {
-	r.DELETE(prefix, func(c *gin.Context) {
-		qsid := c.Query("id")
-		if len(qsid) == 0 {
-			c.String(400, "expect id but not found")
-			return
-		}
-		id, err := strconv.Atoi(qsid)
-
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		var item = new(T)
-		item, err = database.GetItem(db, item, uint(id))
-		/* 保持接口的幂等性 */
-		if err != nil {
-			c.JSON(200, gin.H{
-				"id": id,
-			})
-			return
-		}
-		err = database.DeleteItem(db, new(T), uint(id))
-		if err != nil {
-			c.String(500, err.Error())
-		} else {
-			c.JSON(200, item)
-		}
-		// return
-	})
-}
-
-func PUTItem[T any](r *gin.Engine, db *gorm.DB, prefix string) {
-	r.PUT(prefix, func(c *gin.Context) {
-		qsid := c.Query("id")
-		if len(qsid) == 0 {
-			c.String(400, "expect id but not found")
-			return
-		}
-		id, err := strconv.Atoi(qsid)
-
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-		var item = new(T)
-		err = c.ShouldBindJSON(&item)
-		if err != nil {
-			c.String(400, err.Error())
-			return
-		}
-
-		err = database.UpdateItem(db, &item, uint(id))
-		if err != nil {
-			c.String(500, err.Error())
-			return
-		}
-		item, err = database.GetItem(db, item, uint(id))
-		if err != nil {
-			c.String(404, err.Error())
-		} else {
-			c.JSON(200, item)
-		}
-		// return
-	})
+	controllers.DELETEItem[T](r, db, prefix)
+	controllers.PUTItem[T](r, db, prefix)
 }
