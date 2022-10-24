9107fc7c6addae805e7f10f81ec21904fe7ff23b
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-24 12:12:15 +0800

    Update POSTItem.go

--- backend/controllers/POSTItem.go
+++ backend/controllers/POSTItem.go
@@ -6,32 +6,60 @@ import (
 	"gorm.io/gorm"
 )
 
+type TWO[T any, Y any] struct {
+	First  T
+	Second Y
+}
+
 func POSTItem[T any](r *gin.Engine, createDB func() *gorm.DB, prefix string, model *T) {
 	r.POST(prefix, func(c *gin.Context) {
-		var input map[string]any
-		err := c.ShouldBindJSON(&input)
+		var inputs []map[string]any
+		err := c.ShouldBindJSON(&inputs)
 		if err != nil {
 			c.String(400, err.Error())
 			return
 		}
-		delete(input, "id")
 
-		var item = database.MapToStruct[T](input)
-		id, err := database.CreateItem(createDB, model, item)
-		if err != nil {
-			c.String(500, err.Error())
-			return
-		} else {
-
-			res, err := database.GetItem(createDB, model, uint(id))
-			if err != nil {
-				c.JSON(200, []map[string]interface{}{})
-			} else {
-				c.JSON(200, []map[string]interface{}{res})
-			}
-			return
-
+		for _, input := range inputs {
+			delete(input, "id")
 		}
-		// return
+		var ch = make(chan TWO[map[string]any, error])
+
+		for _, input := range inputs {
+
+			go func(input map[string]any, ch chan TWO[map[string]any, error]) {
+
+				var item = database.MapToStruct[T](input)
+				id, err := database.CreateItem(createDB, model, item)
+				if err != nil {
+					ch <- TWO[map[string]any, error]{nil, err}
+					return
+				} else {
+
+					res, err := database.GetItem(createDB, model, uint(id))
+					if err != nil {
+						ch <- TWO[map[string]any, error]{nil, err}
+						return
+					} else {
+						ch <- TWO[map[string]any, error]{res, nil}
+						return
+					}
+
+				}
+			}(input, ch)
+		}
+		var results = []map[string]interface{}{}
+		for range inputs {
+			two := <-ch
+			res := two.First
+			err := two.Second
+			if err != nil {
+				c.String(400, err.Error())
+				return
+			}
+			results = append(results, res)
+		}
+		c.JSON(200, results)
+
 	})
 }
