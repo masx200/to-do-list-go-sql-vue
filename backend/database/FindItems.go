a69efe33ebc8b2baf8d0aa79a3f2e739d2d31fbb
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-24 10:59:18 +0800

    Update FindItems.go

--- backend/database/FindItems.go
+++ backend/database/FindItems.go
@@ -2,13 +2,17 @@ package database
 
 import (
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 func FindItems[T any](db *gorm.DB, limit int, page int, model *T, query *T, order string, direction string) ([]map[string]any, error) {
 
+	if direction == "desc" {
+		db = db.Order(order + " " + "DESC")
+	} else {
+		db = db.Order(order + " " + "ASC")
+	}
 	var items = []map[string]any{}
-	result := db.Model(model).Omit("deleted_at").Where(query).Limit(limit).Offset(page * limit).Order(clause.OrderByColumn{Column: clause.Column{Name: order}, Desc: direction == "desc"}).Find(&items)
+	result := db.Model(model).Omit("deleted_at").Where(query).Limit(limit).Offset(page * limit).Find(&items)
 
 	return items, result.Error
 }
