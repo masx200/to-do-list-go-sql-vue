776630d8e5fc9e37e17ac07f9b9b72298999bbd0
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-24 13:29:05 +0800

    Update GetItem.go

--- backend/database/GetItem.go
+++ backend/database/GetItem.go
@@ -1,14 +1,21 @@
 package database
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 )
 
 func GetItem[T any](createDB func() *gorm.DB, model *T, id uint) (map[string]interface{}, error) {
 	db := createDB()
 
-	res := map[string]interface{}{}
-	result := db.Model(&model).Omit("deleted_at").First(res, id)
-
-	return res, result.Error
+	users := []map[string]interface{}{}
+	result := db.Limit(1).Model(&model).Omit("deleted_at").Where("id = ?", id).Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	if len(users) > 0 {
+		return users[0], nil
+	}
+	return nil, errors.New("record not found")
 }
