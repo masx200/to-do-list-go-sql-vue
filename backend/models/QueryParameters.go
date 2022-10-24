1a826933fd913f3c1e26533e3b4350e294db5de5
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-24 10:42:30 +0800

    Update QueryParameters.go

--- backend/models/QueryParameters.go
+++ backend/models/QueryParameters.go
@@ -2,8 +2,8 @@ package models
 
 type QueryParameters struct {
 	ID        uint   ` form:"id"`
-	Limit     int   ` form:"limit"`
-	Page      int   ` form:"page"`
+	Limit     int    ` form:"limit"`
+	Page      int    ` form:"page"`
 	Order     string ` form:"order"`
-	Direction string   ` form:"direction"`
+	Direction string ` form:"direction"`
 }
