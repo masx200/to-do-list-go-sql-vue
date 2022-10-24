dfd71139cd3e98e2ca2a28e077315e7de78e8018
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-24 13:57:11 +0800

    PUTItem,UpdateItem

--- backend/controllers/PUTItem.go
+++ backend/controllers/PUTItem.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"strconv"
-
 	"gitee.com/masx200/to-do-list-go-sql-vue/backend/database"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -10,35 +8,69 @@ import (
 
 func PUTItem[T any](r *gin.Engine, createDB func() *gorm.DB, prefix string, model *T) {
 	r.PUT(prefix, func(c *gin.Context) {
-		qsid := c.Query("id")
-		if len(qsid) == 0 {
-			c.String(400, "expect id but not found")
-			return
-		}
-		id, err := strconv.Atoi(qsid)
 
+		var inputs []map[string]any
+		err := c.ShouldBindJSON(&inputs)
 		if err != nil {
 			c.String(400, err.Error())
 			return
 		}
-		var item = new(T)
-		err = c.ShouldBindJSON(&item)
-		if err != nil {
-			c.String(400, err.Error())
-			return
+		var ch = make(chan TWO[map[string]any, error])
+		var output = func(res map[string]any, err error) {
+			ch <- TWO[map[string]any, error]{res, err}
+		}
+		for _, input := range inputs {
+			qsid, o := input["id"]
+			if !o {
+				c.String(400, "expect id but not found")
+				return
+			}
+			id, ok := qsid.(float64)
+
+			if !ok {
+				c.String(400, err.Error())
+				return
+			}
+			var item = input
+			go func(id float64, item map[string]any) {
+
+				err := database.UpdateItem(createDB, model, item, uint(id))
+				/* 保持接口的幂等性 */
+				if err != nil {
+
+					output(gin.H{
+						"id": id,
+					}, nil)
+
+					return
+				}
+				res, err := database.GetItem(createDB, model, uint(id))
+				if err != nil {
+					output(nil, err)
+
+					return
+
+				} else {
+					output(res, nil)
+
+					return
+
+				}
+			}(id, item)
 		}
 
-		err = database.UpdateItem(createDB, &item, uint(id))
-		if err != nil {
-			c.String(500, err.Error())
-			return
+		var results = []map[string]interface{}{}
+		for range inputs {
+			two := <-ch
+			res := two.First
+			err := two.Second
+			if err != nil {
+				c.String(500, err.Error())
+				return
+			}
+			results = append(results, res)
 		}
-		res, err := database.GetItem(createDB, item, uint(id))
-		if err != nil {
-			c.String(500, err.Error())
-		} else {
-			c.JSON(200, []map[string]any{res})
-		}
-		// return
+		c.JSON(200, results)
+
 	})
 }
--- backend/database/UpdateItem.go
+++ backend/database/UpdateItem.go
@@ -4,10 +4,10 @@ import (
 	"gorm.io/gorm"
 )
 
-func UpdateItem[T any](createDB func() *gorm.DB, item *T, id uint) error {
+func UpdateItem[T any](createDB func() *gorm.DB, model *T, item map[string]any, id uint) error {
 
 	db := createDB()
-	var model T
+
 	result := db.Model(&model).Where("id = ?", id).Select("*").Omit("id", "created_at", "deleted_at").Updates(&item)
 
 	return result.Error
