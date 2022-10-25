1e1f8d971075b5a32e36a9ed1bf9aba5da3e0aa1
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-25 15:34:30 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,20 +5,28 @@ import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-    ],
-    server: {
-        port: 4173,
-    },
-    preview: {
-        port: 4173,
-    },
+export default defineConfig(({ mode, command }) => {
+    const isdrop = mode === "production" && command === "build";
+
+    return {
+        esbuild: {
+            legalComments: "none",
+            drop: isdrop ? ["console", "debugger"] : undefined,
+        },
+        plugins: [
+            vue(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
+        server: {
+            port: 4173,
+        },
+        preview: {
+            port: 4173,
+        },
+    };
 });
