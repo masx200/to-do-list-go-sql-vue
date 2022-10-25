d9df921902a57cb57602079c6253089533e47146
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-25 19:32:58 +0800

    格式化

---
+++ frontend/src/app.ts
@@ -0,0 +1,74 @@
+import { defineComponent, onMounted, ref } from "vue";
+import authorInput from "./author-input.vue";
+import { createItem } from "./controllers/createItem";
+import { deleteItems } from "./controllers/deleteItems";
+import { listItems, ToDoItemFull } from "./controllers/listItems";
+
+export default defineComponent({
+    components: { authorInput },
+    setup(props, ctx) {
+        onMounted(() => {
+            onquery();
+        });
+        const author = ref("");
+        function onchange(target: string): void {
+            author.value = target;
+        }
+
+        async function onsubmit(event: Event) {
+            event.preventDefault();
+
+            await createItem({
+                author: author.value,
+                completed: false,
+                content: content.value,
+            });
+
+            await onquery();
+        }
+
+        const content = ref("");
+
+        const listdata = ref([] as ToDoItemFull[]);
+
+        const limit = 20;
+
+        const page = ref(0);
+
+        const direction = "desc";
+
+        const order = "id";
+
+        async function onquery() {
+            listdata.value = await listItems({
+                order,
+                direction,
+                page: page.value,
+                limit,
+            });
+        }
+        const multipleSelection = ref<ToDoItemFull[]>([]);
+        const handleSelectionChange = (val: ToDoItemFull[]) => {
+            multipleSelection.value = val;
+        };
+        async function ondelete() {
+            if (multipleSelection.value.length === 0) return;
+            await deleteItems(
+                multipleSelection.value.map((a) => ({ id: a.id }))
+            );
+            multipleSelection.value = [];
+            await onquery();
+        }
+        return {
+            onquery,
+            ondelete,
+            handleSelectionChange,
+            author,
+            onchange,
+            content,
+            onsubmit,
+            page,
+            listdata,
+        };
+    },
+});
