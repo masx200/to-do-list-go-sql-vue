4959f8d5fae83c2e0406953df99e95f4f0a061cd
masx200 <masx200@qq.com>
2022-12-06 16:24:14 +0800

    Update DeleteItems.go

--- backend/database/DeleteItems.go
+++ backend/database/DeleteItems.go
@@ -6,14 +6,6 @@ import (
 
 func DeleteItems[T any](db *gorm.DB, model *T, ids []uint) error {
 	db = CloneDB(db)
-	// defer CloseDB(db)
+
 	return db.Model(&model).Delete(model, ids).Error
 }
-
-// func CloseDB(db *gorm.DB) error {
-// 	sqlDB, err := db.DB()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return sqlDB.Close()
-// }
